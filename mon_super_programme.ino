68c723cdf8d5300e72c354ba546224ffe6a0dd45
NicHub <nicolas.jeanmonod@gmail.com>
2017-11-17 20:33:21 +0100

    super modif 2000

--- mon_super_programme.ino
+++ mon_super_programme.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Blink
+*/
 
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
